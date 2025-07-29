5ec11abe336356537ac57768bbf98c1f0816fde2
burbalas <154432698+burbalas@users.noreply.github.com>
2025-07-29 10:08:39 -0700

    casino_st
    start

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f0f22929fb3c34915365b69cd66f8faa2484091")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7798088486098bffb47f0d298c58b68fa6e8b7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
